5da65eb30f8c9b0c76fd7d31a9317d033f07f155
Bianco Veigel <zivillian@eventphone.de>
2019-02-16 13:56:10 +0100

    add md5 to SYS_LICENSE_TIMER

--- RfpProxy.Log/Messages/SysLicenseTimerMessage.cs
+++ RfpProxy.Log/Messages/SysLicenseTimerMessage.cs
@@ -11,20 +11,23 @@ namespace RfpProxy.Log.Messages
 
         public TimeSpan GracePeriod { get; }
 
-        public ReadOnlyMemory<byte> Reserved2 { get; }
+        /// <summary>
+        /// md5sum of binary PARK
+        /// </summary>
+        public ReadOnlyMemory<byte> Md5 { get; }
 
         public SysLicenseTimerMessage(ReadOnlyMemory<byte> data) : base(MsgType.SYS_LICENSE_TIMER, data)
         {
             Reserved1 = Raw.Slice(0, 2);
             var grace = BinaryPrimitives.ReadUInt16BigEndian(Raw.Slice(2).Span);
             GracePeriod = TimeSpan.FromMinutes(grace);
-            Reserved2 = Raw.Slice(4);
+            Md5 = Raw.Slice(4);
         }
 
         public override void Log(TextWriter writer)
         {
             base.Log(writer);
-            writer.Write($"Grace Period({GracePeriod}) Reserved1({HexEncoding.ByteToHex(Reserved1.Span)}) Reserved2({HexEncoding.ByteToHex(Reserved2.Span)})");
+            writer.Write($"Grace Period({GracePeriod}) Reserved1({HexEncoding.ByteToHex(Reserved1.Span)}) Md5({HexEncoding.ByteToHex(Md5.Span)})");
         }
     }
 }
