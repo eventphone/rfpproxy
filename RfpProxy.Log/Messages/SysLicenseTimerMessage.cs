d93ba109df189c856ea75ddcff6b01ce1965f597
Bianco Veigel <zivillian@eventphone.de>
2019-02-16 13:32:31 +0100

    output unknown SYS_LICENSE_TIMER bytes

--- RfpProxy.Log/Messages/SysLicenseTimerMessage.cs
+++ RfpProxy.Log/Messages/SysLicenseTimerMessage.cs
@@ -7,20 +7,24 @@ namespace RfpProxy.Log.Messages
 {
     public sealed class SysLicenseTimerMessage : AaMiDeMessage
     {
+        public ReadOnlyMemory<byte> Reserved1 { get; }
+
         public TimeSpan GracePeriod { get; }
+
+        public ReadOnlyMemory<byte> Reserved2 { get; }
 
         public SysLicenseTimerMessage(ReadOnlyMemory<byte> data) : base(MsgType.SYS_LICENSE_TIMER, data)
         {
-            var grace = BinaryPrimitives.ReadUInt16BigEndian(base.Raw.Slice(2).Span);
+            Reserved1 = Raw.Slice(0, 2);
+            var grace = BinaryPrimitives.ReadUInt16BigEndian(Raw.Slice(2).Span);
             GracePeriod = TimeSpan.FromMinutes(grace);
+            Reserved2 = Raw.Slice(4);
         }
-
-        protected override ReadOnlyMemory<byte> Raw => base.Raw.Slice(2);
 
         public override void Log(TextWriter writer)
         {
             base.Log(writer);
-            writer.Write($"Grace Period: {GracePeriod} Reserved: {HexEncoding.ByteToHex(Raw.Span)}");
+            writer.Write($"Grace Period({GracePeriod}) Reserved1({HexEncoding.ByteToHex(Reserved1.Span)}) Reserved2({HexEncoding.ByteToHex(Reserved2.Span)})");
         }
     }
 }
