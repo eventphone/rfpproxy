7d65a3e76336155996f369e0aeb34c95d9cfc08e
Bianco Veigel <zivillian@eventphone.de>
2019-03-09 12:41:04 +0100

    fix MacConInd PMID

--- RfpProxy.Log/Messages/Dnm/MacConIndPayload.cs
+++ RfpProxy.Log/Messages/Dnm/MacConIndPayload.cs
@@ -7,28 +7,25 @@ namespace RfpProxy.Log.Messages.Dnm
 {
     public sealed class MacConIndPayload : DnmPayload
     {
-        public byte Reserved1 { get; }
-
         /// <summary>
         /// Portable part MAC Identity / OMM PPn
         /// </summary>
-        public ushort PMID { get; }
+        public uint PMID { get; }
 
-        public ReadOnlyMemory<byte> Reserved2 { get; }
+        public ReadOnlyMemory<byte> Reserved { get; }
 
         public override bool HasUnknown => true;
 
         public MacConIndPayload(ReadOnlyMemory<byte> data) : base(data)
         {
             var span = data.Span;
-            Reserved1 = span[0];
-            PMID = BinaryPrimitives.ReadUInt16BigEndian(span.Slice(1));
-            Reserved2 = data.Slice(3);
+            PMID = (uint) (((span[0] & 0xf) << 16) | BinaryPrimitives.ReadUInt16BigEndian(span.Slice(1)));
+            Reserved = data.Slice(3);
         }
         
         public override void Log(TextWriter writer)
         {
-            writer.Write($" Reserved1({Reserved1:x2}) PMID({PMID:x5}) Reserved2({Reserved2.ToHex()})");
+            writer.Write($" PMID({PMID:x5}) Reserved({Reserved.ToHex()})");
         }
     }
 }
