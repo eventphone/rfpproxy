5eb2b818cb035bb08f31f0ba6cd7f5d2c3361c7f
Bianco Veigel <zivillian@eventphone.de>
2019-03-13 20:40:21 +0100

    comments

--- RfpProxy.Log/Messages/Nwk/InformationElements/Proprietary/DeTeWe/DisplayDeTeWeElement.cs
+++ RfpProxy.Log/Messages/Nwk/InformationElements/Proprietary/DeTeWe/DisplayDeTeWeElement.cs
@@ -8,8 +8,19 @@ namespace RfpProxy.Log.Messages.Nwk.InformationElements.Proprietary.DeTeWe
 {
     public class DisplayDeTeWeElement : DeTeWeElement
     {
+        /// <summary>
+        /// byte 2 always seems to be 0xff
+        /// byte 1:
+        /// 0xb2 - can't scroll
+        /// 0xb3 - scrollable
+        /// 0xb8 - add bottom
+        /// 0xb7 - add top
+        /// </summary>
         public ushort Reserved { get; }
 
+        /// <summary>
+        /// seems to be always 2 entries
+        /// </summary>
         public List<string> Values { get; }
 
         public override bool HasUnknown => true;
