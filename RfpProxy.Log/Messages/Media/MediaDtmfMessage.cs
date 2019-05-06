d3301c5845f7549f1d1a622eb2ed58154064292d
Bianco Veigel <zivillian@eventphone.de>
2019-05-06 19:42:42 +0200

    decode MEDIA_DTMF

--- RfpProxy.Log/Messages/Media/MediaDtmfMessage.cs
+++ RfpProxy.Log/Messages/Media/MediaDtmfMessage.cs
@@ -1,31 +1,31 @@
 ﻿using System;
+using System.Buffers.Binary;
 using System.IO;
-using RfpProxyLib;
 
 namespace RfpProxy.Log.Messages.Media
 {
     public sealed class MediaDtmfMessage : MediaMessage
     {
-        public ReadOnlyMemory<byte> Reserved1 { get; }
+        public ushort Duration { get; }
 
         public char Key { get; }
 
-        public ReadOnlyMemory<byte> Reserved2 { get; }
+        public MediaDirection Direction { get; }
 
-        public override bool HasUnknown => true;
+        protected override ReadOnlyMemory<byte> Raw => base.Raw.Slice(4);
 
         public MediaDtmfMessage(ReadOnlyMemory<byte> data) : base(MsgType.MEDIA_DTMF, data)
         {
-            Reserved1 = Raw.Slice(0, 2);
-            var key = Raw.Span[2];
-            Key = (char) key;
-            Reserved2 = Raw.Slice(3);
+            var span = base.Raw.Span;
+            Duration = BinaryPrimitives.ReadUInt16LittleEndian(span);
+            Key = (char) span[2];
+            Direction = (MediaDirection) span[3];
         }
 
         public override void Log(TextWriter writer)
         {
             base.Log(writer);
-            writer.Write($"Reserved1({Reserved1.ToHex()}) Key({Key}) Reserved2({Reserved2.ToHex()})");
+            writer.Write($"Duration({Duration}ms) Key({Key}) Direction({Direction})");
         }
     }
 }
