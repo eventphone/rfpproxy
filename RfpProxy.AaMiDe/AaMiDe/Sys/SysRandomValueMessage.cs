61e0eb457a7b5e8f377da0c21540aa415f813b36
Bianco Veigel <zivillian@eventphone.de>
2023-01-06 20:28:22 +0100

    add SysRandomValueMessage

---
+++ RfpProxy.AaMiDe/AaMiDe/Sys/SysRandomValueMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.IO;
+
+namespace RfpProxy.AaMiDe.Sys
+{
+    public sealed class SysRandomValueMessage : AaMiDeMessage
+    {
+        public uint Random { get; }
+
+        public override bool HasUnknown => false;
+
+        protected override ReadOnlyMemory<byte> Raw => base.Raw.Slice(4);
+
+        public SysRandomValueMessage(ReadOnlyMemory<byte> data) : base(MsgType.SYS_RANDOM_VALUE, data)
+        {
+            Random = BinaryPrimitives.ReadUInt32BigEndian(base.Raw.Span);
+        }
+
+        public override void Log(TextWriter writer)
+        {
+            base.Log(writer);
+            writer.Write($"Random({Random:X})");
+        }
+    }
+}
